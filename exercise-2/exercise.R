d9685a9ea1c65e5fd49245b96c95669389ffd972
Jaren Tilley <jtilley1999@gmail.com>
2017-10-10 14:26:52 -0700

    did exercises

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,17 +1,16 @@
 # Exercise 2: Subsetting and Manipulating Vectors
 
 # Create a vector `x` that the numbers 5,2,6,2,1,7
-
+x <- c(5,2,6,2,1,7)
 
 # Create a vector `y` that has the numbers 2,3
-
+y <- c(2,3)
 
 # Create a vector `z` by adding (not combining, but adding) `x` and `y` (note the recycling!)
+z <- x + y
 
-
-# Create a vector `first.three` that has the first three elements of `z` in it
-
-
+# Create a vector `first.three` that has the first three elements of `z` in
+first.three <- z[c(1:3)]
 # Create a vector `small` that has the values of `z` that are less than 5
 
 
